d5e6b4d9fcc5b640f06be0690fada698a0bf0ccf
rmstcv <rmstcv@gmail.com>
2024-12-27 18:07:51 +0300

    prettier added

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  const notes = [
+    {
+      nick: "note-nick-1",
+      name: "Note 1",
+      description: "Description of note 1..",
+    },
+    {
+      nick: "note-nick-2",
+      name: "Note 2",
+      description: "Description of note 2..",
+    },
+    {
+      nick: "note-nick-3",
+      name: "Note 3",
+      description: "Description of note 3..",
+    },
+    {
+      nick: "note-nick-4",
+      name: "Note 4",
+      description: "Description of note 4..",
+    },
+    {
+      nick: "note-nick-5",
+      name: "Note 5",
+      description: "Description of note 5..",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>Notenick</h1>
+      {notes.map((note) => {
+        return (
+          <div key={note.nick}>
+            <h2>{note.name}</h2>
+            <p>{note.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
