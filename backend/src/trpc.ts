ec611396ffa74c7d514d4fd7e4cae01723d821d7
rmstcv <rmstcv@gmail.com>
2025-01-08 19:25:06 +0300

    trpc added to backend

---
+++ backend/src/trpc.ts
@@ -0,0 +1,39 @@
+import { initTRPC } from '@trpc/server'
+
+const notes = [
+  {
+    nick: 'note-nick-1',
+    name: 'Note 1',
+    description: 'Description of note 1..',
+  },
+  {
+    nick: 'note-nick-2',
+    name: 'Note 2',
+    description: 'Description of note 2..',
+  },
+  {
+    nick: 'note-nick-3',
+    name: 'Note 3',
+    description: 'Description of note 3..',
+  },
+  {
+    nick: 'note-nick-4',
+    name: 'Note 4',
+    description: 'Description of note 4..',
+  },
+  {
+    nick: 'note-nick-5',
+    name: 'Note 5',
+    description: 'Description of note 5..',
+  },
+]
+
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getNotes: trpc.procedure.query(() => {
+    return notes
+  }),
+})
+
+export type TrpcRouter = typeof trpcRouter
