4fe444bf6c9ea5bcf43ef67cac25185ad82ebca8
rmstcv <rmstcv@gmail.com>
2025-01-22 23:08:46 +0300

    lodash added

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,38 +1,19 @@
 import { initTRPC } from '@trpc/server'
 
-const notes = [
-  {
-    nick: 'note-nick-1',
-    name: 'Note 1',
-    description: 'Description of note 1..',
-  },
-  {
-    nick: 'note-nick-2',
-    name: 'Note 2',
-    description: 'Description of note 2..',
-  },
-  {
-    nick: 'note-nick-3',
-    name: 'Note 3',
-    description: 'Description of note 3..',
-  },
-  {
-    nick: 'note-nick-4',
-    name: 'Note 4',
-    description: 'Description of note 4..',
-  },
-  {
-    nick: 'note-nick-5',
-    name: 'Note 5',
-    description: 'Description of note 5..',
-  },
-]
+import _ from 'lodash'
+
+const notes = _.times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Note ${i}`,
+  description: `Description of note ${i}...`,
+  text: _.times(100, (j) => `<p>Text paragrph ${j} of note ${i}...</p>`).join(''),
+}))
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getNotes: trpc.procedure.query(() => {
-    return { notes }
+    return { notes: notes.map((note) => _.pick(note, ['nick', 'name', 'description'])) }
   }),
 })
 
